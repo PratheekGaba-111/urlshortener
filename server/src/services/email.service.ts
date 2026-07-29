94d885bd38d53ac6a87483a7d530052f674a3a1f
Pratheek-111 <prathu031105@gmail.com>
2026-07-30 04:37:11 +0530

    fixed sers

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -2,14 +2,16 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
-
 // Check SMTP connection when server starts
 transporter.verify()
     .then(() => {
