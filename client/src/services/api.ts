df74990081b543942918263dd5f29b5086da5468
Pratheek-111 <prathu031105@gmail.com>
2026-07-30 02:58:05 +0530

    Implemented new UI and also fixed some auth issues

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearAuthToken, TOKEN_KEY } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
