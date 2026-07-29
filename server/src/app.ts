e97b30b46308174f680c34b459669c337ff0b46f
Pratheek-111 <prathu031105@gmail.com>
2026-07-30 04:04:33 +0530

    Fixed proxy

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authLimiter);
 app.use("/api/url", urlLimiter);
-
+app.set("trust proxy", 1);
 // Health checkk
 app.get("/", (req, res) => {
     res.json({
--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
+    windowMs: 3 * 60 * 1000,
     limit: 5,
     message: {
         message: "Too many login attempts. Try again later."
